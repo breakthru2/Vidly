dcf56899266a7b0806b48ee413055c5fbfc66ee6
Paul Breakthrough <paulbreakthru@outlook.com>
2018-10-04 19:50:36 +0100

    Update to Customer

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -11,12 +11,15 @@ namespace Vidly.Models
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }
-        [Required]
+        public string Name { get; set; }        
         public Genre Genre { get; set; }
+        [Required]
+        [Display(Name ="Genre")]
         public byte GenreId { get; set; }
         public DateTime DateAdded { get; set; }
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        [Display(Name ="Number in Stock")]
         public byte NumberInStock { get; set; }
 
 
