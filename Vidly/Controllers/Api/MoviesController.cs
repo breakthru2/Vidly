475e8abae5e24c68a3b7b9db07c59e6d0cb01517
Paul Breakthrough <paulbreakthru@outlook.com>
2018-10-15 11:04:31 +0100

    Customer Web Api

---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //GET /api/movies
+        public IEnumerable<MovieDto> GetMovie()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+        }
+        //GET /api/movies/id
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            movieDto.Id = movie.Id;
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+        }
+    }
+}
